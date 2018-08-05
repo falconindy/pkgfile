43b5b5eaabc23abcb1f0bf554a113036d367bdae
Dave Reisner <dreisner@archlinux.org>
2018-08-05 13:06:47 -0400

    move license to top level file in repo, install with software
    ditch this noise from the individual files

--- src/missing.h
+++ src/missing.h
@@ -1,10 +1,7 @@
-#ifndef _MISSING_H
-#define _MISSING_H
+#pragma once
 
 #ifndef HAVE_MEMPCPY
 static inline void *mempcpy(void *dst, const void *src, size_t len) {
   return (void *)(((char *)memcpy(dst, src, len)) + len);
 }
-#endif
-
 #endif
