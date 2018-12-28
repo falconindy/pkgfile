5629d1da2ea06f681ded3deb8dfad31210a7a09f
Dave Reisner <dreisner@archlinux.org>
2018-12-28 13:37:00 -0500

    fixup return status from command_not_found hooks
    We should probably retain the 127 exit status for when commands aren't
    found, rather than masking them.
    
    Closes #43.

--- extra/command-not-found.zsh
+++ extra/command-not-found.zsh
@@ -5,9 +5,9 @@ command_not_found_handler() {
   if [[ -n "$pkgs" ]]; then
     printf '%s may be found in the following packages:\n' "$cmd"
     printf '  %s\n' $pkgs[@]
-    return 0
+  else
+    printf 'zsh: command not found: %s\n' "$cmd" 1>&2
   fi
 
-  printf 'zsh: command not found: %s\n' "$cmd" 1>&2
   return 127
 }
